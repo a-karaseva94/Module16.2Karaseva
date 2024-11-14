4363ad472f835f0803063f79a52f32baf6dc54eb
a-karaseva94 <a-karaseva94@mail.ru>
2024-11-14 12:43:19 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -17,16 +17,16 @@ async def admin_page():
     return "Вы вошли как администратор"
 
 
-# маршрут к страницам пользователей, используя параметр в пути
+# маршрут к страницам пользователей (id)
 @app.get("/user/{user_id}")
-async def user_page(
+async def user_page_1(
         user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]):
     return f"Вы вошли как пользователь № {user_id}"
 
 
-# маршрут к страницам пользователей, передавая данные в адресной строке
+# маршрут к страницам пользователей (имя, возраст)
 @app.get("/user/{username}/{age}")
-async def user_page_address_str(
+async def user_page_2(
         username: Annotated[str, Path(min_length=5, max_length=20,
                                       description="Enter username", example="UrbanUser")],
         age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> dict:
